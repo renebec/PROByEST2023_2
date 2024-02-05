a947d9802384177705fad533c79a65cf8df6599c
renebec <39104197+renebec@users.noreply.github.com>
2024-02-05 15:29:08 -0600

    Add files via upload

--- docs/actions.js
+++ docs/actions.js
@@ -1,15 +1,4 @@
-var images = document.getElementsByTagName("img");
-for (var i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", function () {
-        for (var j = 0; j < images.length; j++) {
-            if (i != j) {
-                images[j].style.transform = "scale(1)";
-            }
-          }
-        this.style.transform = "scale(1.3)";
-        this.style.margin='40px'
-    });
-}
+
 
 function PopWindowOne()
 {
