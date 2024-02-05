ad5f44d6e44e086e60e7a0846f8d407ff44530cd
renebec <39104197+renebec@users.noreply.github.com>
2024-02-05 16:39:36 -0600

    Add files via upload

--- docs/actions.js
+++ docs/actions.js
@@ -9,3 +9,8 @@ function PopWindowTwo()
 {
     floatingWindow = window.open("edades.html","","height=400,width=300,scrollbars=no");
 }
+
+function PopWindowThree()
+{
+    floatingWindow = window.open("r_first_scripts.html","","height=400,width=300,scrollbars=no");
+}
